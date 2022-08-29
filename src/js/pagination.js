062956cecae9a3fdfdf162d01e4397f57f041283
Andrii Kulyk <andrij.kulyk@gmail.com>
2022-08-29 12:13:59 +0300

    3

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -2,16 +2,24 @@ import pagination from './pagination';
 const pagContainer = document.querySelector('.pag-container');
 
 function createPagination(totalPages, pagesShown, startPage) {
-  const startBtnsMarkup = `<div class="pag-btn to-start">На початок</div><div id="${pagesShown}" class="pag-btn move-left">...</div><div class="pag-btns-container"></div><div id="${pagesShown}" class="pag-btn move-right">...</div><div class="pag-btn to-end">В кінець</div>`;
+  const startBtnsMarkup = `<div class="left-constrols-container"><div class="pag-btn to-start">На початок</div><div id="${pagesShown}" class="pag-btn move-left">...</div></div><div class="pag-btns-container"></div><div class="right-controls-container"><div id="${pagesShown}" class="pag-btn move-right">...</div><div class="pag-btn to-end">В кінець</div></div>`;
 
   pagContainer.innerHTML = startBtnsMarkup;
 
   const pagBtnsContainer = document.querySelector('.pag-btns-container');
+  const letftControlsContainer = document.querySelector(
+    '.letft-controls-container'
+  );
+  const rightControlsContainer = document.querySelector(
+    '.right-controls-container'
+  );
 
   let btnsArray = [];
   let current_page = startPage;
   let firstPage = startPage;
   let lastPage = pagesShown;
+
+  console.log(firstPage, lastPage);
 
   function createBtnsArray(value) {
     for (let i = 1; i <= value; i += 1) {
@@ -26,8 +34,7 @@ function createPagination(totalPages, pagesShown, startPage) {
 
   function appendPagBtnsMarkup(array, counter, page) {
     pagBtnsContainer.innerHTML = '';
-    page--;
-    let btnsMarkup = '';
+    // page--;
 
     let start = counter * page;
     let end = start + counter;
@@ -56,6 +63,18 @@ function createPagination(totalPages, pagesShown, startPage) {
 
     activeBtn.classList.remove('pag-btn-active');
     evt.target.classList.add('pag-btn-active');
+  }
+
+  rightControlsContainer.addEventListener('click', onRightControlsClick);
+
+  function onRightControlsClick(evt) {
+    console.log(evt.target);
+
+    if (evt.target.classList.contains('.move-right')) {
+      current_page = startPage + pagesShown;
+
+      appendPagBtnsMarkup(btnsArray, pagesShown, current_page);
+    }
   }
 }
 
